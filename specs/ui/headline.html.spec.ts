d75934fd4496afe47977c95bca368e5c69c3671a
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-08-16 21:10:19 +0200

    test(headline.html.ts): reshape tests structure a little bit

--- specs/ui/headline.html.spec.ts
+++ specs/ui/headline.html.spec.ts
@@ -1,11 +1,11 @@
 import { describe, test, expect } from "@jest/globals";
-import { headlineHtml } from "../src/ui/headline.html";
+import { headlineHtml } from "../../src/ui/headline.html";
 
-describe("UI helpers", () => {
+describe("UI Headline html", () => {
   const inputText = "Some text";
   const h1Text = "<h1>Some text</h1>";
   const h2Text = "<h2>Some text</h2>";
-
+  const h4Text = "<h4>Some text</h4>";
   test("headlineHtml H1 test", async () => {
     expect(headlineHtml(inputText, 1)).toEqual(h1Text);
   });
@@ -14,5 +14,7 @@ describe("UI helpers", () => {
     expect(headlineHtml(inputText, 2)).toEqual(h2Text);
   });
 
-
+  test("headlineHtml H4 test", async () => {
+    expect(headlineHtml(inputText, 4)).toEqual(h4Text);
+  });
 });
