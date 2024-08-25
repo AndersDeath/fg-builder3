622116ece38332dd7931cdceed70b4d61a569cb0
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-08-25 21:59:16 +0200

    test(utils.ts): update tests for utils

--- specs/utils.spec.ts
+++ specs/utils.spec.ts
@@ -1,13 +1,16 @@
 import { describe, test, expect } from "@jest/globals";
-import { Utils } from "../src/utils";
 import { utilsSpecMock } from "./mocks/utils.mock";
+import {
+  removeIgnoreBlock,
+  replaceGlobalImagePathToLocal,
+  replaceMarkdownPageBreakToHtml,
+} from "../src/utils";
 
 describe("Utils class", () => {
-  const utils = new Utils();
   const mock = utilsSpecMock;
   test("replaceGlobalImagePathToLocal test", async () => {
     expect(
-      await utils.replaceGlobalImagePathToLocal(
+      await replaceGlobalImagePathToLocal(
         mock.replaceGlobalImagePathToLocal.input
       )
     ).toEqual(mock.replaceGlobalImagePathToLocal.output);
@@ -15,18 +18,15 @@ describe("Utils class", () => {
 
   test("replaceMarkdownPageBreakToHtml test", async () => {
     expect(
-      await utils.replaceMarkdownPageBreakToHtml(
+      await replaceMarkdownPageBreakToHtml(
         mock.replaceMarkdownPageBreakToHtml.input
       )
     ).toEqual(mock.replaceMarkdownPageBreakToHtml.output);
   });
 
   test("removeIgnoreBlock test", async () => {
-    expect(
-      await utils.removeIgnoreBlock(
-        mock.removeIgnoreBlock.input
-      )
-    ).toEqual(mock.removeIgnoreBlock.output);
+    expect(await removeIgnoreBlock(mock.removeIgnoreBlock.input)).toEqual(
+      mock.removeIgnoreBlock.output
+    );
   });
-
 });
