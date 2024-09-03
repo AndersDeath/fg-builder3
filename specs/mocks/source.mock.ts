14d5e3aad6c818f31267f5d547b826959f5e91e6
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-09-03 21:50:07 +0200

    test(mock): start creation of mocks of the full test of the system

---
+++ specs/mocks/source.mock.ts
@@ -0,0 +1,13 @@
+const file1 = "";
+
+const file2 = "";
+
+const folder = {
+  "somefirst.md": file1,
+  "somesecond.md": file2,
+};
+
+const files = {
+  category1: folder,
+  category2: folder,
+};
