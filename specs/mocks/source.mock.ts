ff5e364628e4b7c1e02583be24d559c0a9012796
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-09-05 21:00:56 +0200

    feat(mock): update mock data

--- specs/mocks/source.mock.ts
+++ specs/mocks/source.mock.ts
@@ -1,12 +1,12 @@
 import { file1Mock } from "./file1.mock";
 import { file2Mock } from "./file2.mock";
 
-const folder = {
+const folderMock = {
   "somefirst.md": file1Mock,
   "somesecond.md": file2Mock,
 };
 
-const files = {
-  category1: folder,
-  category2: folder,
+const filesMock = {
+  category1: folderMock,
+  category2: folderMock,
 };
